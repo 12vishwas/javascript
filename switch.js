4308a246d605622016f7081e411f46a330bbc6e4
12vishwas <134786056+12vishwas@users.noreply.github.com>
2023-06-16 10:40:45 +0530

    switch statement

---
+++ switch.js
@@ -0,0 +1,28 @@
+//switch statement in js
+var day = "sun";
+
+switch(day){
+
+    case "mon": console.log("Today is monday");
+    break;
+
+    case "tuesday": console.log("Today is tuesday");
+    break;
+
+    case "wednesday": console.log("Today is wednesday");
+    break;
+
+    case "thursday": console.log("Today is thurday");
+    break;
+
+    case "friday": console.log("today is friday");
+    break;
+
+    case "saturday": console.log("Today is saturday");
+    break;
+
+    case "sunday": console.log("Today is sunday");
+    break;
+
+    default: console.log("invalid");
+}
