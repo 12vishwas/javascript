9fd7360f0c22854df4f23ced8664d51aa654c098
12vishwas <134786056+12vishwas@users.noreply.github.com>
2023-06-16 10:21:05 +0530

    if-else statement

---
+++ if-else.js
@@ -0,0 +1,11 @@
+//if-else statement in js
+
+var a=5,b=6;
+
+if(a+b > 11){
+    console.log("greater than 11")
+    console.log("yes")}
+else if(a+b < 11)
+    console.log("less than 11")
+else
+    console.log('equal')
