bfaf05f21a4884dbd5fdbdc4470eb548096744bc
12vishwas <134786056+12vishwas@users.noreply.github.com>
2023-06-17 13:24:42 +0530

    non primitive data type

---
+++ objects.js
@@ -0,0 +1,25 @@
+//non primitive data type
+
+//object- collection of key value pairs enclosed within {}
+var fruits = {
+    'apple':'red',
+    'mango':'yellow',
+    Eat: function(){
+        console.log(`${this.apple} is eating by shanky`)
+    }
+     
+}
+console.log(fruits['apple'])
+console.log(fruits.mango)
+fruits.Eat()
+
+
+var obj ={
+    'name':'akshay',
+    'roll': 1,
+    'id': 102,
+    marks(){
+        console.log(`${this.name} got 52 marks`)
+    }
+}
+obj.marks()
