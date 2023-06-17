b6a4851f39bfed23283e8677801bf55afe84c0c9
12vishwas <134786056+12vishwas@users.noreply.github.com>
2023-06-17 14:40:45 +0530

    functions

---
+++ func.js
@@ -0,0 +1,11 @@
+//first method to declare function
+function sing(){
+    console.log('she is singing')
+}
+sing()
+
+//another method to declare function
+var eat = function(){
+    console.log('he is eating')
+}
+eat()
