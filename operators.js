b7aeecea708ccfa03c0077ce060a3787cce976a8
12vishwas <134786056+12vishwas@users.noreply.github.com>
2023-06-17 11:16:32 +0530

    operators

---
+++ operators.js
@@ -0,0 +1,18 @@
+//bitwise operators
+var a=5,b=6
+console.log(a&b)
+console.log(a|b)
+console.log(a^b)
+console.log(~a)
+console.log(~b)
+
+//logical operators
+var a =true,b=false
+console.log(a&&b)
+console.log(a||b)
+console.log(!a)
+console.log(!b)
+
+//ternary operator:it is an alternative of if else 
+var a=3
+console.log((a==3)?console.log('ok'):console.log('not ok'))
